ce3ecb3bd9fcd857892741e426c67b85f3695d7d
mimie <mileusnic.milos97@gmail.com>
2022-05-16 00:52:19 -0400

    the basic structure of the car is ready, working on advance feature

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         public static void main(String[] args) {
             // write your code here
             basicFeatures car =new basicFeatures();
-
+            
         }
     }
